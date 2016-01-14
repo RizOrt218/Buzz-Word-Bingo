9c2d005f1a63eaa6171491918c836ffe8b974a68
RizOrt218 <rizhiafalen@gmail.com>
2016-01-14 00:00:17 -1000

    wip creating cases for post

--- server.js
+++ server.js
@@ -1,38 +1,52 @@
 var express    = require( 'express' );
-var server     = express();
 var bodyParser = require( 'body-parser' );
+var server     = express();
 
 server.use( express.static( 'public' ));
 server.use(bodyParser.urlencoded({ extended: true}));
 
-var words = [];
+var buzzObjectArr = [];
 var score = 0;
 
 server.get('/', function (req, res) {
-  res.send( 'herro' );
+  res.render( './index.html' );
 });
 
-
 server.get('/buzzwords', function (req, res) {
-  res.send({ buzzwords: words });
+  res.send({ buzzwords: buzzObjectArr });
 });
 
 server.route('/buzzword')
   .post( function (req, res) {
-    //if buzzword already exist, don't
-    //add it and message to use put.
-    words.push( req.body );
-    score += parseInt(req.body.points);
-    console.log(req.body.points);
-    console.log( 'score',score );
-    res.send({'success' : true });
+
+    var reqBody = req.body.buzzWord;
+    //check for duplicate before pushing b word to array
+    for( var i = 0; i < buzzObjectArr.length; i++ ){
+      if( buzzObjectArr[i].buzzWord !== reqBody ) {
+        buzzObjectArr.push( req.body );
+        console.log( 'ARE YOU WORKING!' );
+
+        //creating body?
+        var body = {
+          'buzzWord' : reqBody,
+          'points' : req.body.points,
+          'heard' : true
+        };
+
+        res.send({ 'success' : true });
+      }
+    }
   })
 
   .put( function (req, res) {
-    //if bw is same update points
+
+    var reqBody = req.body;
+
+    score += parseInt(req.body.points);
+
     res.send({
       'success' : true,
-      'newScore' : points
+      'newScore' : score
     });
   })
 
@@ -43,7 +57,7 @@ server.route('/buzzword')
   });
 var server = server.listen( 8080, function ( ) {
   var host = 'localhost';
-  var port = server.address().port;
+  var port = 8080;
 
   console.log( 'Example server listening at http://%s:%s', host, port );
 });
